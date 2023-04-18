026ddb1953f6e3432586965734c741d5bcb8460f
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-18 14:54:17 +0900

    ログイン機能とログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, presence: true
   validates :first_name_kana, :last_name_kana,format: {with: /[^ -~｡-ﾟ]+/,message: ' is invalid. Input full-width katakana characters'}
   validates :first_name,:last_name,format: {with: /[^ -~｡-ﾟ]+/,message: ' is invalid. Input full-width characters'}
-  validates :password, format: {with: /[^ -~｡-ﾟ]+/,message: ' is invalid. Include both letters and numbers'}
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 end
