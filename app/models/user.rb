596806cb4c38ee8cccf92017a442f98266aac8ce
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-21 17:26:51 +0900

    ユーザー管理機能のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name
     validates :last_name
   end  
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :first_name_kana
     validates :last_name_kana
   end  
