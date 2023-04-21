065cd840153cdb4453e8aabd1c0cdba788bde4d1
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-21 16:39:43 +0900

    テストコード修正後

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, presence: true
-  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: ' is invalid. Input full-width characters' }
-  validates :first_name_kana, :last_name_kana,format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters' }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end  
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 end
