167075abdec96ee6c16737577bb5c778cff7b347
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-23 20:44:54 +0900

    ルーティングの設定　バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,12 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_day
   belongs_to :category
+
+  belongs_to :user
   has_one_attached :image
 
   validates :name, :description, :price, presence: true
 
-  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id, numericality: { other_than: 1 , message: "can't be blank" }
 
 end
