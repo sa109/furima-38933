f3a9d3ee0bb582b7e3e2428cac6fb5b5427ea3b1
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-23 21:26:49 +0900

    new/create定義　ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, notice: '保存ができませんでした'
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).parmit(:name, :description, :image, :status_id, :postage_id, :region_id, :shipping_day_id, :category_id).merge(user_id: current_user.id)
   end
 
   
