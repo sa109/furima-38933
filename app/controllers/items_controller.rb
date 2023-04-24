f1f132ade1da8fa9e8b152937f83327f0b8f1937
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-24 18:55:04 +0900

    index/new/create修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,18 +2,19 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
+    @items = Item.includes(:user)
   end
 
   def new
-    @item = Item.new
+    @items = Item.new
   end
   
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path 
     else
-      render :new, notice: '保存ができませんでした'
+      render :new
     end
   end
 
@@ -22,5 +23,5 @@ class ItemsController < ApplicationController
     params.require(:item).parmit(:name, :description, :image, :status_id, :postage_id, :region_id, :shipping_day_id, :category_id).merge(user_id: current_user.id)
   end
 
-  
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, :description, presence: true
+
+  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
   belongs_to :postage
   belongs_to :region
   belongs_to :shipping_day
   belongs_to :category
-
-  belongs_to :user
-  has_one_attached :image
-
-  validates :name, :description, :price, presence: true
-
-  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id, numericality: { other_than: 1 , message: "can't be blank" }
 
 end
