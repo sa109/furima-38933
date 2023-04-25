a75d9f5187cf81f892840937cb30d9f3a86f67aa
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-25 20:25:12 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :description, presence: true
+  validates :name, :description, :price, presence: true
 
-  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id,
-            numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id,numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
@@ -13,4 +13,4 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_day
   belongs_to :category
-end
+end 
