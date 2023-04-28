211e61fc022c5e4426f3ddd43d6aa0934fe6121b
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-28 14:40:15 +0900

    異常系テストコード記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ApplicationRecord
 
   validates :name, :description, :price, :image, presence: true
 
-  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id,numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  validates :status_id, :region_id, :postage_id, :shipping_day_id, :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
