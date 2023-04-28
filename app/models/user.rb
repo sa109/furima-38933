545a84a56a5553f8038e62e5cdd24f9db8f09ec3
ぺんた <penta@pentanoMacBook-Air.local>
2023-04-28 21:47:03 +0900

    出品一覧機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :items
+  has_many :purchases
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, presence: true
