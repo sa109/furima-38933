78f76d85de815ffbf99615066a71a3a3e2d99155
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-02 18:14:59 +0900

    updateアクション定義修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to ietm_path(item_params)
+      redirect_to item_path(item_params)
     else
       render :edit
     end
