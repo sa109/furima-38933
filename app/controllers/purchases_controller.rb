99e3243671550529a6767dfcf17fac162efa59c8
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-07 20:56:39 +0900

    purchaseコントローラー修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,13 +3,13 @@ class PurchasesController < ApplicationController
   end
 
   def new
-    @purchase_shippingday = PurchaseShippingDay.new
+    @purchase_shippingadres = PurchaseShippingAdres.new
   end
 
   def create
-    @purchase_shippingday = PurchaseShippingDay.new(purchase_params)
-    if @purchase_shippingday.valid?
-      @purchase_shippingday.save
+    @purchase_shippingadres = PurchaseShippingAdres.new(purchase_params)
+    if @purchase_shippingadres.valid?
+      @purchase_shippingadres.save
       redirect_to root_path
     else
       render :index
@@ -19,6 +19,6 @@ class PurchasesController < ApplicationController
   private
  
   def purchase_params
-    params.require(:purchase_shippingday).permit(:post_code, :region_id, :city, :block, :building, :phone_number).maerge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_shippingadres).permit(:post_code, :region_id, :city, :block, :building, :phone_number).maerge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
