93113ae0687a1a2e751e341c1132269743cc6f1c
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-08 17:39:52 +0900

    purchaseコントローラー修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,24 +1,22 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!
-
-
+ 
   def index
-    @purchase_shippingadres = PurchaseShippingAdres.new
+    @purchase_shipping_adres = PurchaseShippingAdres.new
   end
 
   def create
-    @purchase_shippingadres = PurchaseShippingAdres.new(purchase_params)
-    if @purchase_shippingadres.valid?
-       @purchase_shippingadres.save
+    @purchase_shipping_adres = PurchaseShippingAdres.new(purchase_params)
+    if @purchase_shipping_adres.valid?
+       @purchase_shipping_adres.save
        return redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
  
   def purchase_params
-    params.require(:purchase_shippingadres).permit(:post_code, :region_id, :city, :block, :building, :phone_number).maerge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_shipping_adres).permit(:post_code, :region_id, :city, :block, :building, :phone_number).maerge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
