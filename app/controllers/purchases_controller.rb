6bc063ebfdd8c718cf168b93b84eee5eab4b7017
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-10 21:30:42 +0900

    purchaseコントローラー設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,11 @@
 class PurchasesController < ApplicationController
- 
+ before_action :move_to_index, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @purchase_shipping_adres = PurchaseShippingAdres.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_shipping_adres = PurchaseShippingAdres.new(purchase_params)
     if @purchase_shipping_adres.valid?
        @purchase_shipping_adres.save
@@ -21,4 +20,9 @@ class PurchasesController < ApplicationController
   def purchase_params
     params.require(:purchase_shipping_adres).permit(:post_code, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+  end
+
 end
