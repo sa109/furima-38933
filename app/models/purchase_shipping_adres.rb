635449995f2c1d40d69fa2690522c850931ccc3a
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-12 21:08:13 +0900

    バリデーション設定

--- app/models/purchase_shipping_adres.rb
+++ app/models/purchase_shipping_adres.rb
@@ -5,14 +5,16 @@ class PurchaseShippingAdres
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :region_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :block
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid"}
+  end
+
+  with_options presence: true do
+    validates :phone_number, length: { minimum: 10, maximum:11, message: "is too short"}, format: {with: /\A[0-9]{11}\z/, message: "is invalid. Input only number"}
   end
   
-
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     ShippingAdres.create(post_code: post_code, region_id: region_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
