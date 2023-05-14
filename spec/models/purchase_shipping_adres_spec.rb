40e51f7fe57a63359b3b4c489cb66cb4fba3a284
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-14 20:57:59 +0900

    異常系テストコード記述とjavascriptのビュー記述

--- spec/models/purchase_shipping_adres_spec.rb
+++ spec/models/purchase_shipping_adres_spec.rb
@@ -26,8 +26,8 @@ RSpec.describe PurchaseShippingAdres, type: :model do
         @purchase_shipping_adres.region_id = '2'
         expect(@purchase_shipping_adres).to be_valid
       end
-      it '番地が空でなければ保存できる' do
-        @purchase_shipping_adres.block = '旭区１２３'
+      it '市区町村が空でなければ保存できる' do
+        @purchase_shipping_adres.city = '市原市'
         expect(@purchase_shipping_adres).to be_valid
       end
       it '番地が空でなければ保存ができる' do
@@ -41,6 +41,69 @@ RSpec.describe PurchaseShippingAdres, type: :model do
       it '電話番号が11桁以内かつハイフンなしであれば保存できる' do
         @purchase_shipping_adres.phone_number = 12_345_678_910
         expect(@purchase_shipping_adres).to be_valid
+      end
+    end
+
+    context '配送先情報が保存できないとき' do
+      it 'user_idが空だと保存できない' do
+        @purchase_shipping_adres.user_id = nil
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @purchase_shipping_adres.item_id = nil
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号が空だと保存できない' do
+        @purchase_shipping_adres.post_code = nil
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Post code can't be blank")
+      end
+      it '郵便番号がハイフンがなければ保存ができない' do
+        @purchase_shipping_adres.post_code = '1_234_123'
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Post code is invalid. Enter it as follows (e.g. 123-4567)")
+      end
+      it '都道府県が「---」だと登録されない' do
+        @purchase_shipping_adres.region_id = 1
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Region can't be blank")
+      end
+      it '市区町村が空だと保存できない' do
+        @purchase_shipping_adres.city = nil
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空では保存できない' do
+        @purchase_shipping_adres.block = nil
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Block can't be blank")
+      end
+      it '電話番号が空だと保存できない' do 
+        @purchase_shipping_adres.phone_number = nil
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号がハイフンがあると保存できない' do
+        @purchase_shipping_adres.phone_number = '123-1234-1212'
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+      it '電話番号が９桁以下だと保存できない' do
+        @purchase_shipping_adres.phone_number = '12345678'
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Phone number is too short")
+      end
+      it '電話番号が12桁以上だと保存できない' do
+        @purchase_shipping_adres.phone_number = '12_3456_789_012'
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Phone number is too short", "Phone number is invalid. Input only number")
+      end
+      it '電話番号が全角だと保存できない' do
+        @purchase_shipping_adres.phone_number = '１２＿２３４＿５６７＿８８８'
+        @purchase_shipping_adres.valid?
+        expect(@purchase_shipping_adres.errors.full_messages).to include("Phone number is too short", "Phone number is invalid. Input only number")
       end
     end
   end
