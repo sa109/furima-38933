d891183fd3335ecb8e6f28134be135328eec7a5c
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-15 22:27:22 +0900

    コントローラー・ビューを編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,12 +8,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_shipping_adres = PurchaseShippingAdres.new(purchase_params)
     if @purchase_shipping_adres.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: purchase_params(:token),
-        currency:'jpy'
-      )
+      pay_item
        @purchase_shipping_adres.save
        return redirect_to root_path
     else
@@ -25,6 +20,15 @@ class PurchasesController < ApplicationController
  
   def purchase_params
     params.require(:purchase_shipping_adres).permit(:post_code, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                   # 通貨の種類（日本円）
+    )
   end
 
   def move_to_index
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,10 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_519f479e208b2f12805ef520')// PAY.JPテスト公開鍵
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY)// PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
+
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
