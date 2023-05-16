760c34cb309f2dedacc2cfebe99b539fcae7d9f8
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-16 18:57:39 +0900

    itemsコントローラーのeditアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
 
   def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
-    if current_user.id == @item.user_id
+    if @item.user_id == current_user.id && @item.order.nil?
     else
       redirect_to root_path
     end
