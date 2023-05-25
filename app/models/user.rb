1cd9df2a1dd6a9f0d64a10a47348269c65d1acbb
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-25 20:56:09 +0900

    commentモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :items
   has_many :purchases
+  has_many :comments
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
