ddc028564ef7853ea277af6dae698f1ba75d19c4
ぺんた <penta@pentanoMacBook-Air.local>
2023-05-27 19:33:06 +0900

    comment#create編集

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,6 +1,6 @@
 class CommentChannel < ApplicationCable::Channel
   def subscribed
-    
+    stream_form "comment_channels"
   end
 
   def unsubscribed
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
       redirect_to item_path(params[:item_id])
     end
   end
