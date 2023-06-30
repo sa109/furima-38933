45881eca8fc659226eae180b1aebbe5db7d3b37a
ぺんた <penta@pentanoMacBook-Air.local>
2023-06-30 11:00:53 +0900

    自動デプロイ設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :application, 'furima-38933'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:sa109/furima-38933.git'
 
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
